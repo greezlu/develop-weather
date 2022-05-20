8100f04aedef3f816bb874798ba13f0999cade54
Serhii Shapoval <dev2662web@gmail.com>
2022-05-20 09:48:10 +0300

    Add frontend layout handle with ViewModel

---
+++ app/code/Develop/Weather/ViewModel/WeatherStatusData.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Develop\Weather\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Develop\Weather\Model\WeatherResolver;
+use Develop\Weather\Api\WeatherStatusRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class WeatherStatusData implements ArgumentInterface
+{
+    /**
+     * @var WeatherStatusRepositoryInterface
+     */
+    private WeatherStatusRepositoryInterface $weatherStatusRepository;
+
+    /**
+     * @var WeatherResolver
+     */
+    private WeatherResolver $weatherResolver;
+
+    /**
+     * @param WeatherStatusRepositoryInterface $weatherStatusRepository
+     * @param WeatherResolver $weatherResolver
+     */
+    public function __construct(
+        WeatherStatusRepositoryInterface $weatherStatusRepository,
+        WeatherResolver $weatherResolver
+    ) {
+        $this->weatherStatusRepository = $weatherStatusRepository;
+        $this->weatherResolver = $weatherResolver;
+    }
+
+    /**
+     * Get last weather status data.
+     *
+     * @return array
+     */
+    public function getWeatherData(): array
+    {
+        try {
+            $weatherStatus = $this->weatherStatusRepository->getLast();
+        } catch (NoSuchEntityException $error) {
+            return [];
+        }
+
+        $cityCoordinatesId      = $weatherStatus->getCityCoordinatesId();
+        $weatherInterpreterId   = $weatherStatus->getWeatherInterpreterId();
+
+        return [
+            'City'                  => $this->weatherResolver->getCityName($cityCoordinatesId),
+            'Weather Description'   => $this->weatherResolver->getWeatherDescription($weatherInterpreterId),
+            'Temperature'           => $weatherStatus->getTemperature(),
+            'Wind Speed'            => $weatherStatus->getWindSpeed(),
+            'Wind Direction'        => $weatherStatus->getWindDirection()
+        ];
+    }
+}
