cabe489d0ad89bf5d13bf40f5403fd3403468404
Serhii Shapoval <dev2662web@gmail.com>
2022-05-20 09:45:37 +0300

    Add frontend action-controller

---
+++ app/code/Develop/Weather/Controller/Index/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Develop\Weather\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $pageFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @inheridoc
+     */
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
