73e6b939fba8294e76fe4d88e96ff14835e0de04
Serhii Shapoval <dev2662web@gmail.com>
2022-05-20 09:55:34 +0300

    Add cron job

---
+++ app/code/Develop/Weather/Cron/GetLastWeather.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Develop\Weather\Cron;
+
+use Develop\Weather\Model\WeatherUpdater;
+use Develop\Weather\Api\Data\WeatherStatusInterface;
+use Develop\Weather\Api\Data\WeatherStatusInterfaceFactory;
+use Develop\Weather\Api\WeatherStatusRepositoryInterface;
+
+class GetLastWeather
+{
+    /**
+     * @var WeatherUpdater
+     */
+    private WeatherUpdater $weatherUpdater;
+
+    /**
+     * @var WeatherStatusInterfaceFactory
+     */
+    private WeatherStatusInterfaceFactory $weatherStatusFactory;
+
+    /**
+     * @var WeatherStatusRepositoryInterface
+     */
+    private WeatherStatusRepositoryInterface $weatherStatusRepository;
+
+    /**
+     * @param WeatherUpdater $weatherUpdater
+     * @param WeatherStatusInterfaceFactory $weatherStatusFactory
+     * @param WeatherStatusRepositoryInterface $weatherStatusRepository
+     */
+    public function __construct(
+        WeatherUpdater $weatherUpdater,
+        WeatherStatusInterfaceFactory $weatherStatusFactory,
+        WeatherStatusRepositoryInterface $weatherStatusRepository
+    ) {
+        $this->weatherUpdater = $weatherUpdater;
+        $this->weatherStatusFactory = $weatherStatusFactory;
+        $this->weatherStatusRepository = $weatherStatusRepository;
+    }
+
+    public function execute(): void
+    {
+        $currentData = $this->weatherUpdater->getCurrentWeather();
+
+        if (empty($currentData)) {
+            return; //@todo log
+        }
+
+        /** @var WeatherStatusInterface $weatherStatusModel */
+        $weatherStatusModel = $this->weatherStatusFactory->create();
+
+        $weatherStatusModel->setCityCoordinatesId($currentData['city_coordinates_id']);
+        $weatherStatusModel->setWeatherInterpreterId($currentData['weather_interpreter_id']);
+        $weatherStatusModel->setTemperature($currentData['temperature']);
+        $weatherStatusModel->setWindSpeed($currentData['wind_speed']);
+        $weatherStatusModel->setWindDirection($currentData['wind_direction']);
+        $weatherStatusModel->setCreatedAt($currentData['created_at']);
+
+        try {
+            $this->weatherStatusRepository->save($weatherStatusModel);
+        } catch (\Exception $error) {
+            //@todo log
+        }
+    }
+}
