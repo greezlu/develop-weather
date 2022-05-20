92b287aeec48545f75b2eefb21dc650d6ec222f5
Serhii Shapoval <dev2662web@gmail.com>
2022-05-20 09:49:24 +0300

    Add admin area action-controller

---
+++ app/code/Develop/Weather/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Develop\Weather\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+    const ADMIN_RESOURCE = 'Develop_Weather::weather_log';
+
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $pageFactory;
+
+    /**
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        PageFactory $pageFactory
+    ) {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
